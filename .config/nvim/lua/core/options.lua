aa3290f3ea2b289420d20b2264b3fccf06708925
Ruyi Lan <rliu2400@gmail.com>
2025-08-16 13:03:38 -0700

    yeah

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -10,5 +10,20 @@ vim.o.splitright = true
 -- Python host program
 vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
 
-vim.g.mapleader = " " -- Set leader key to Space
-vim.g.maplocalleader = " " -- Also set local leader to Space
+-- Leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- this is alr in statusline
+vim.o.showmode = false
+
+-- undo history
+vim.o.undofile = true
+
+-- highlight the curr line?
+-- vim.o.cursorline = true -- nah
+vim.o.scrolloff = 10
+
+-- clear highlights by pressing esc in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
